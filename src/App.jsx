b34e77eea95934e2898aa53c11042b3355411078
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-03-19 10:35:46 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Route, Routes } from "react-router-dom";
 import { Signup } from "./Components/Signup";
 import { Login } from "./Components/Login";
 import axios from "axios";
+import { Userdashboard } from "./Components/user/Userdashboard";
+import { AdminDashboard } from "./Components/Admin/AdminDashboard";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:3001/api";
@@ -15,6 +17,10 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Userdashboard />}></Route>
+        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+        
+        
       </Routes>
     </>
   );
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,20 +1,62 @@
 import React from 'react'
 import '../assets/css/Login.css'
+import { useForm } from 'react-hook-form'
+import axios from 'axios';
+import{ useNavigate } from 'react-router-dom';
 export const Login = () => {
+   const Navigate = useNavigate()
+   const {register,handleSubmit,formState:{errors}} = useForm();
+   const SubmitHandler = async (data) =>{
+      try{
+          const res = await axios.post("/user/login",data);
+          if(res.status == 201){
+            alert("Login Success");
+            localStorage.setItem("id",res.data.data._id);
+            localStorage.setItem("role",res.data.data.roleId.name);
+            if(res.data.data.roleId.name === "User"){
+               Navigate("/dashboard")
+            }else if(res.data.data.roleId.name === "Admin") {
+                Navigate("/admin-dashboard")
+            }
+          }
+      }catch(error){
+         alert("Login Failed");
+      }
+   }
+   const ErrorHandler = {
+      emailHandler:{
+         required:{
+            value:true,
+            message:"The Email is required"
+         },
+         pattern:{
+            value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message:"Please Enter The Valid Email Address"
+         }
+      },
+      passwordHandler:{
+         required:{
+            value:true,
+            message:"The Password is required"
+         }
+      }
+   }
   return (
     <div>
      <div class="wrapper">
          <div class="title">
             Login Form
          </div>
-         <form action="#">
+         <form onSubmit={handleSubmit(SubmitHandler)}>
             <div class="field">
-               <input type="text" required />
+               <input type="text"  {...register("email",ErrorHandler.emailHandler)} />
                <label>Email Address</label>
+               {errors.email?.message}
             </div>
             <div class="field">
-               <input type="password" required />
+               <input type="password" {...register("password",ErrorHandler.passwordHandler)} />
                <label>Password</label>
+               {errors.password?.message}
             </div>
             <div class="content">
                <div class="checkbox">
